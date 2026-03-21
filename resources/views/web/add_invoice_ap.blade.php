9a82396b77e999097f64ecb5997d00832ab2f2f1
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-03-21 18:38:29 +0530

    Fix invoice due date input format mismatch in browser validation

--- resources/views/web/add_invoice_ap.blade.php
+++ resources/views/web/add_invoice_ap.blade.php
@@ -123,9 +123,9 @@
                                     class="form-control datepicker @error('due_date') is-invalid @enderror"
                                     id="exampleInputEmail1" aria-describedby="emailHelp"
                                     value="{{ old('due_date', date('d-m-Y')) }}"
-                                    placeholder="dd-mm-yyyy"
+                                    placeholder="dd-mm-yyyy or dd-mm-yy"
                                     autocomplete="due_date"
-                                    pattern="\\d{2}-\\d{2}-\\d{4}" />
+                                    />
 
                                    
                                 @error('due_date')
