7094e8bf416933146b40c4a9d17a4f7493e5f28c
Meenakshi <nanta1811@gmail.com>
2026-03-21 20:48:42 +0530

    alert popup

--- resources/views/web/add_ap_payments.blade.php
+++ resources/views/web/add_ap_payments.blade.php
@@ -291,7 +291,7 @@
 
             // Check if Paid Amount is greater than Amount to Pay
             if (paidAmount > allowedAmount) {
-                const message = `Amount Paying should not exceed  ${allowedAmount.toFixed(2)} (Outstanding).`;
+                const message = `Amount Paying should not exceed  ${allowedAmount.toFixed(2)} (Outstanding)git status.`;
                 paidAmountInput.setCustomValidity(message);
                 paidAmountInput.classList.add('is-invalid'); // Add invalid class for styling
                 if (showAlert) {
